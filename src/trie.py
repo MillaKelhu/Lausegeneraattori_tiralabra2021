377459e719944c121e57ed9a93a87ec55c25a9b0
Milla Kelhu <milla.kelhu@gmail.com>
2021-09-17 14:52:50 +0300

    Jätä luokka TrieSolmu pois, tee testausta, lisää oma tehtävä testien suorittamiselle

--- src/trie.py
+++ src/trie.py
@@ -1,9 +1,39 @@
+from alipuu import alipuu_lauseesta
+
 class Trie:
-    """Luokka jolla toteutetaan trie-tietorakenne
-
-    Attributes:
-
+    """Luokka jolla toteutetaan trie-tietorakenne.
     """
 
     def __init__(self):
-        pass
+        """Luokan konstruktori, joka luo juuren trie-puulle tyhjänä sanakirjana.
+        """
+
+        self.__juuri = {}
+
+    def hae_puu(self):
+        """Palauttaa koko trie-puun sanakirjana juuresta lähtien.
+
+        Returns:
+            dict: Koko trie-puu sanakirjana.
+        """
+        return self.__juuri
+
+    def tyhjenna_puu(self):
+        """Tyhjentää trie-puun kaikista solmuista.
+
+        Returns:
+            bool: Palauttaa arvon True.
+        """
+
+        self.__juuri = {}
+        return True
+
+    def lisaa_lapsi(self, lapsi):
+        if type(lapsi)==str:
+            pass
+                 
+                                         
+
+if __name__ == "__main__":
+    puu = Trie()
+    print(puu.hae_puu())
