d853c2d01ede4f64d6cd6e1234f96f1bc43cf386
Milla Kelhu <milla.kelhu@gmail.com>
2021-09-18 21:35:37 +0300

    Tee tirapuulle toimiva funktio lasten lisäämiseen, poista tarpeettomat funktiot

---
+++ src/testit/trie_test.py
@@ -0,0 +1,40 @@
+import unittest
+from trie import Trie
+
+class TestTrie(unittest.TestCase):
+    def setUp(self):
+        self.puu = Trie()
+
+    def test_hae_puu_palauttaa_puun_oikein(self):
+        vastaus = self.puu.hae_puu()
+
+        self.assertEqual(vastaus, {})
+
+    def test_lisaa_lapsi_lisaa_lauseen_oikein(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        vastaus = self.puu.hae_puu()
+
+        self.assertEqual(vastaus, {'Punainen': {'kissa': {'hyppäsi': {'pöydälle': {}}}}})
+
+    def test_lisaa_lapsi_lisaa_yli_yksi_lausetta_oikein(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        self.puu.lisaa_lapsi('Punainen kuu on kummallinen näky')
+        vastaus = self.puu.hae_puu()
+
+        self.assertEqual(vastaus, {'Punainen': {'kissa': {'hyppäsi': {'pöydälle': {}}}, 'kuu': {'on': {'kummallinen': {'näky': {}}}}}})
+
+    def test_lisaa_lapsi_ei_lisaa_kaksoiskappaleita(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        vastaus = self.puu.hae_puu()
+
+        self.assertEqual(vastaus, {'Punainen': {'kissa': {'hyppäsi': {'pöydälle': {}}}}})
+
+    def tyhjenna_puu_tyhjentaa_puun_oikein(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        self.puu.tyhjenna_puu()
+        vastaus = self.puu.hae_puu()
+
+        self.assertEqual(vastaus, {})
+
+
--- src/trie.py
+++ src/trie.py
@@ -1,5 +1,3 @@
-from alipuu import alipuu_lauseesta
-
 class Trie:
     """Luokka jolla toteutetaan trie-tietorakenne.
     """
@@ -28,12 +26,34 @@ class Trie:
         self.__juuri = {}
         return True
 
-    def lisaa_lapsi(self, lapsi):
-        if type(lapsi)==str:
-            pass
-                 
-                                         
+    def lisaa_lapsi(self, lapsi: str, vanhempi=None):
+        n = len(lapsi)
+
+        if vanhempi == None:
+            vanhempi = self.__juuri
+
+        sana = lapsi
+
+        for i in range(n):
+            if lapsi[i] == ' ':
+                sana = lapsi[:i]
+                lapsi = lapsi[i+1:]
+                break
+
+        if sana not in vanhempi:
+            vanhempi[sana] = {}
+        
+        if sana != lapsi:
+            self.lisaa_lapsi(lapsi, vanhempi[sana])
+
+        return True
 
 if __name__ == "__main__":
     puu = Trie()
+    print(puu.hae_puu())
+    puu.lisaa_lapsi("Punainen kissa hyppäsi pöydälle")
+    print(puu.hae_puu())
+    puu.lisaa_lapsi("Punainen kuu on kummallinen näky")
+    print(puu.hae_puu())
+    puu.lisaa_lapsi("Punainen kissa joi maitoa")
     print(puu.hae_puu())
