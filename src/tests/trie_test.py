2de8d5ab513521344247866c7e11816e6db1a0fb
Milla Kelhu <milla.kelhu@gmail.com>
2021-09-23 11:42:47 +0300

    Lisää funktio lauseen etsimiseen puusta, korjaa testejä

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -28,15 +28,33 @@ class TestTrie(unittest.TestCase):
 
     def test_lisaa_lapsi_ei_lisaa_kaksoiskappaleita(self):
         self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
-        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
-        vastaus = self.puu.hae_puu()
+        vastaus = self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
 
-        self.assertEqual(
-            vastaus, {'Punainen': {'kissa': {'hyppäsi': {'pöydälle': {}}}}})
+        self.assertEqual(vastaus, False)
 
-    def tyhjenna_puu_tyhjentaa_puun_oikein(self):
+    def test_tyhjenna_puu_tyhjentaa_puun_oikein(self):
         self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
         self.puu.tyhjenna_puu()
         vastaus = self.puu.hae_puu()
 
         self.assertEqual(vastaus, {})
+
+    def test_etsi_lause_loytaa_lauseen_oikein(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        self.puu.lisaa_lapsi('Punainen kissa kävi kylvyssä')
+        vastaus = self.puu.etsi_lause('Punainen kissa hyppäsi pöydälle')
+
+        self.assertEqual(vastaus, True)
+
+    def test_etsi_lause_ei_löydä_olematonta_lausetta(self):
+        self.puu.lisaa_lapsi('Punainen kissa kävi_kylvyssä')
+        vastaus = self.puu.etsi_lause('Punainen kissa hyppäsi pöydälle')
+
+        self.assertEqual(vastaus, False)
+
+    def test_etsi_lause_loytaa_osalauseen(self):
+        self.puu.lisaa_lapsi('Punainen kissa hyppäsi pöydälle')
+        self.puu.lisaa_lapsi('Punainen kissa kävi kylvyssä')
+        vastaus = self.puu.etsi_lause('Punainen kissa')
+
+        self.assertEqual(vastaus, True)
--- src/trie.py
+++ src/trie.py
@@ -26,13 +26,34 @@ class Trie:
         self.__juuri = {}
         return True
 
-    def lisaa_lapsi(self, lapsi: str, vanhempi=None):
-        """Lisää lauseen trie-puuhun solmuiksi. 
+    def lisaa_lapsi(self, lapsi: str):
+        """Näkyvä metodi, jolla lause lisätään trie-puuhun solmuiksi.
         Tällä hetkellä erikoismerkkejä ei huomioida laisinkaan.
 
         Args:
             lapsi (str): Lause, joka halutaan lisätä trie-puuhun.
-            vanhempi (vapaaehtoinen): Sanakirja, johon lausetta ollaan 
+
+        Returns:
+            bool: Palauttaa joko arvon True tai False riippuen siitä,
+            onko lause voitu lisätä puuhun.
+        """
+
+        lause_puussa = self.etsi_lause(lapsi)
+
+        if lause_puussa:
+            return False
+
+        return self.__lisaa_lapsi(lapsi)
+
+    def __lisaa_lapsi(self, lapsi: str, vanhempi=None):
+        """Lisää lauseen trie-puuhun solmuiksi.
+        Piilotettu funktio, koska puu menisi sotkuun,
+        jos sanakirjan, johon lause lisätään, saisi valita.
+        Tällä hetkellä erikoismerkkejä ei huomioida laisinkaan.
+
+        Args:
+            lapsi (str): Lause, joka halutaan lisätä trie-puuhun.
+            vanhempi (vapaaehtoinen): Sanakirja, johon lausetta ollaan
             lisäämässä. Oletusarvo on None.
 
         Returns:
@@ -54,6 +75,47 @@ class Trie:
             vanhempi[sana] = {}
 
         if sana != lapsi:
-            self.lisaa_lapsi(lapsi, vanhempi[sana])
+            self.__lisaa_lapsi(lapsi, vanhempi[sana])
 
         return True
+
+    def etsi_lause(self, lause: str, alku=None):
+        """Etsii, onko jokin tietty lause olemassa trie-puussa.
+
+        Args:
+            lause (str): Lause, joka halutaan etsiä trie-puusta.
+            alku (dict, vapaaehtoinen): Sanakirja, josta lähtien lausetta etsitään.
+            Oletusarvo on None.
+
+        Returns:
+            bool: Palauttaa joko arvon False tai True rippuen siitä, onko lause sanakirjassa vai ei.
+        """
+        if alku is None:
+            alku = self.__juuri
+
+        sana = lause
+
+        for i in range(len(lause)):
+            if lause[i] == ' ':
+                sana = lause[:i]
+                lause = lause[i+1:]
+                break
+
+        if sana not in alku:
+            return False
+
+        if sana != lause:
+            return self.etsi_lause(lause, alku[sana])
+
+        return True
+
+
+if __name__ == "__main__":
+    puu = Trie()
+    print(puu.hae_puu())
+    print(puu.lisaa_lapsi("Punainen kissa hyppäsi pöydälle"))
+    print(puu.hae_puu())
+    print(puu.lisaa_lapsi("Punainen kissa hyppäsi pöydälle"))
+    print(puu.hae_puu())
+    print(puu.lisaa_lapsi("Punainen kissa hyppäsi"))
+    print(puu.hae_puu())
